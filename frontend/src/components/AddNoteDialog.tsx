7138a1f5c5fbac879fa1a8201fdc84d662fafccb
shawernseel <shawern101@gmail.com>
2023-05-09 17:27:17 -0700

    added form component

--- frontend/src/components/AddNoteDialog.tsx
+++ frontend/src/components/AddNoteDialog.tsx
@@ -1,19 +1,50 @@
-import { Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 interface AddNoteDialogProps {
     onDismiss: () => void,
 }
 
-const AddNoteDialog = ({onDismiss}: AddNoteDialogProps) => {
-    return ( 
-        <Modal show onHide={onDismiss /* () => onDismiss() */}> {/* hardcode show to true unless onHide*/}
+const AddNoteDialog = ({ onDismiss }: AddNoteDialogProps) => {
+    return (
+        <Modal show onHide={onDismiss /* () => onDismiss() */}> {/* hardcode show to true unless onHide (hoverover onHide for Modal description)*/}
             <Modal.Header closeButton>
                 <Modal.Title>
                     Add Note
                 </Modal.Title>
             </Modal.Header>
+
+            <Modal.Body>
+                <Form id="addNoteForm">
+                    <Form.Group className="mb-3">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Title" //adds text title in field
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Text</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={5}
+                            placeholder="Text"
+                        />
+                    </Form.Group>
+
+                </Form>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button
+                type="submit" //when clicked close
+                form="addNoteForm"
+                >
+                    Save
+                </Button>
+            </Modal.Footer>
         </Modal>
-     );
+    );
 }
- 
+
 export default AddNoteDialog;
